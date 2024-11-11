f2adaa6e7102e496ccaff946d5758eee68ab2246
fran-ink <171171801+fran-ink@users.noreply.github.com>
2024-11-11 10:52:07 -0500

    feat: style basic button
    + add typography and border radius variables

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { classNames } from "../../util/classes";
+import { classNames, variantClassNames } from "../../util/classes";
 
 export interface ButtonProps {
   className?: string;
   onClick?: () => void;
   disabled?: boolean;
   children: React.ReactNode;
-  variant?: "primary" | "secondary" | "outline";
-  size?: "small" | "medium" | "large";
+  variant?: "primary" | "secondary";
+  size?: "sm" | "md";
   type?: "button" | "submit" | "reset";
 }
 
@@ -17,27 +17,24 @@ export const Button: React.FC<ButtonProps> = ({
   disabled = false,
   children,
   variant = "primary",
-  size = "medium",
+  size = "sm",
   type = "button",
 }) => {
   return (
     <button
       type={type}
       className={classNames(
-        "ink-rounded ink-font-medium ink-transition-colors disabled:ink-cursor-not-allowed",
-        {
+        "ink-rounded-full ink-font-medium ink-transition-colors disabled:ink-cursor-not-allowed ink-duration-100",
+        variantClassNames(variant, {
           primary:
-            "ink-bg-primary ink-text-text-on-primary hover:ink-bg-primary-hover disabled:ink-bg-primary-disabled disabled:ink-text-text-on-primary-disabled",
+            "ink-bg-primary ink-text-text-on-primary hover:ink-bg-primary-hover disabled:ink-bg-primary-disabled disabled:ink-text-text-on-primary-disabled active:ink-bg-primary-pressed",
           secondary:
-            "ink-bg-secondary ink-text-text-on-secondary ink-text-white hover:ink-bg-secondary-hover disabled:ink-bg-secondary-disabled disabled:ink-text-text-on-secondary-disabled",
-          outline:
-            "ink-border-2 ink-border-primary ink-text-primary hover:ink-border-primary-hover hover:ink-text-primary-hover disabled:ink-bg-primary-disabled disabled:ink-text-primary-disabled",
-        }[variant],
-        {
-          small: "ink-px-3 ink-py-1 ink-text-sm",
-          medium: "ink-px-4 ink-py-2",
-          large: "ink-px-6 ink-py-3 ink-text-lg",
-        }[size],
+            "ink-bg-secondary ink-text-text-on-secondary hover:ink-bg-secondary-hover disabled:ink-bg-secondary-disabled disabled:ink-text-text-on-secondary-disabled active:ink-bg-secondary-pressed",
+        }),
+        variantClassNames(size, {
+          sm: "ink-px-6 ink-py-4 ink-text-sm ink-text-body-2",
+          md: "ink-px-8 ink-py-4 ink-text-h4",
+        }),
         className
       )}
       onClick={onClick}
