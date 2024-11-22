429bf70eb1acf260b6a907c170f3493978dd8ff2
ink-victor <171172553+ink-victor@users.noreply.github.com>
2024-11-22 11:24:37 -0500

    add missing class

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -13,7 +13,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         className={classNames(
           resetClasses,
-          "ink-w-full ink-rounded-8 ink-bg-background-container ink-p-2 ink-text-body-2",
+          "ink-w-full ink-rounded-8 ink-bg-background-container ink-p-2 ink-text-body-2 ink-text-text-default",
           "focus:ink-outline focus:ink-outline-1 ink-outline-text-on-secondary",
           className
         )}
