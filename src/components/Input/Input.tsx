feb9129a81f24ee147819a99a0c0fa558584d8a5
ink-victor <171172553+ink-victor@users.noreply.github.com>
2024-11-22 11:24:37 -0500

    update for tw4

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,5 @@
 import React, { forwardRef } from "react";
-import { classNames, resetClasses } from "../../util/classes";
+import { classNames } from "../../util/classes";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -12,9 +12,8 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
       <input
         ref={ref}
         className={classNames(
-          resetClasses,
-          "ink-w-full ink-rounded-8 ink-bg-background-container ink-p-2 ink-text-body-2 ink-text-text-default",
-          "focus:ink-outline focus:ink-outline-1 ink-outline-text-on-secondary",
+          "ink:w-full ink:font-default ink:rounded-8 ink:bg-background-container ink:p-2 ink:text-body-2 ink:text-text-default",
+          "focus:ink:outline focus:ink:outline-1 ink:outline-text-on-secondary",
           className
         )}
         {...props}
