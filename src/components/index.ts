698b7b6685c10f930d88663ff048c87d30e5a7da
fran-ink <171171801+fran-ink@users.noreply.github.com>
2024-11-27 14:46:54 -0500

    feat: toggle component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ export * from "./Input";
 export * from "./Modal";
 export * from "./Popover";
 export * from "./SegmentedControl";
+export * from "./Toggle";
 export * from "./Typography";
 export * from "./Wallet";
