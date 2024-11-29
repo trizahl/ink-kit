fc66cc0f6fba8a63b2d75f62ccafefd54683925a
fran-ink <171171801+fran-ink@users.noreply.github.com>
2024-11-29 09:13:29 -0500

    feat: radio group + radio

--- src/components/Checkbox/CheckboxLabel.tsx
+++ src/components/Checkbox/CheckboxLabel.tsx
@@ -1,30 +1,7 @@
-import { Description, Field, Label } from "@headlessui/react";
-import { PropsWithChildren } from "react";
+import { FieldLabel, FieldLabelProps } from "../FieldLabel";
 
-export interface CheckboxLabelProps extends PropsWithChildren {
-  label: React.ReactNode;
-  description?: React.ReactNode;
-}
+export interface CheckboxLabelProps extends FieldLabelProps {}
 
-export const CheckboxLabel: React.FC<CheckboxLabelProps> = ({
-  label,
-  description,
-  children,
-}) => {
-  return (
-    <Field className="ink:flex ink:flex-col ink:font-default">
-      <div className="ink:flex ink:items-center ink:gap-1">
-        {children}
-        <Label className="ink:cursor-pointer ink:h-3 ink:flex ink:items-center ink:justify-center ink:text-body-2 ink:text-text-default">
-          {label}
-        </Label>
-      </div>
-
-      {description && (
-        <Description className="ink:text-caption-2 ink:text-text-default">
-          {description}
-        </Description>
-      )}
-    </Field>
-  );
+export const CheckboxLabel: React.FC<CheckboxLabelProps> = (props) => {
+  return <FieldLabel {...props} />;
 };
